383641b17db41b291c11a1fec8a325dec36783b6
Veyndan Stuart <veyndan@gmail.com>
2023-10-20 20:46:07 +0200

    Remove the internal representation of the board
    This may be useful in the future, but at the momement I don't see the benefit of it.

--- index.js
+++ index.js
@@ -1,74 +1,11 @@
-class Point {
-
-	/**
-	 * @param {?number} player
-	 * @param {number} checkerCount
-	 */
-	constructor(player, checkerCount) {
-		this.player = player;
-		this.checkerCount = checkerCount;
-	}
-}
-
-const points = [
-	new Point(2, 2),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(1, 5),
-	new Point(null, 0),
-	new Point(1, 3),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(2, 5),
-	new Point(1, 5),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(2, 3),
-	new Point(null, 0),
-	new Point(2, 5),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(null, 0),
-	new Point(1, 2),
-];
-
-points.forEach((point, pointIndex) => {
-	if (point.player !== null) {
-		for (let checkerIndex = 0; checkerIndex < point.checkerCount; checkerIndex++) {
-			const checkerElement = document.createElementNS(`http://www.w3.org/2000/svg`, `use`);
-			checkerElement.dataset[`player`] = `${point.player}`;
-			checkerElement.dataset[`point`] = `${pointIndex + 1}`; // 1-based indexing per standard backgammon notation.
-			checkerElement.setAttribute(`href`, `#checker`);
-			checkerElement.style.translate = checkerTranslate(pointIndex, checkerIndex);
-			document.getElementById(`checkers`).append(checkerElement);
-		}
-	}
-});
-
 document.getElementById(`checkers`).childNodes.forEach(checkerElement => {
 	checkerElement.addEventListener(`click`, () => {
 		const dieNumber = 4;
 		const player = Number(checkerElement.dataset[`player`]);
 		const originPointIndex = Number(checkerElement.dataset[`point`]) - 1;
-		const originPoint = points[originPointIndex];
 		const destinationPointIndex = originPointIndex + (player === 1 ? -dieNumber : dieNumber);
-		const destinationPoint = points[destinationPointIndex];
-		checkerElement.style.translate = checkerTranslate(destinationPointIndex, destinationPoint.checkerCount);
-		originPoint.checkerCount -= 1;
-		destinationPoint.checkerCount += 1;
-		checkerElement.dataset[`point`] = `${destinationPointIndex + 1}`
+		const destinationPointCheckerCount = document.querySelectorAll(`use[href="#checker"][data-point="${destinationPointIndex + 1}"]`).length;
+		checkerElement.style.translate = `${(destinationPointIndex < 12 ? -1 : 1) * ((destinationPointIndex % 12 * 40) + (destinationPointIndex % 12 >= 6 ? 50 : 0)) + (destinationPointIndex < 12 ? 490 : 0)}px ${destinationPointIndex < 12 ? (380 - destinationPointCheckerCount * 40) : (destinationPointCheckerCount * 40)}px`;
+		checkerElement.dataset[`point`] = `${destinationPointIndex + 1}`;
 	});
 });
-
-/**
- * @param {number} pointIndex
- * @param {number} checkerIndex
- */
-function checkerTranslate(pointIndex, checkerIndex) {
-	return `${(pointIndex < 12 ? -1 : 1) * ((pointIndex % 12 * 40) + (pointIndex % 12 >= 6 ? 50 : 0)) + (pointIndex < 12 ? 490 : 0)}px ${pointIndex < 12 ? (380 - checkerIndex * 40) : (checkerIndex * 40)}px`;
-}
