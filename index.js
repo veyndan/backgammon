cce5ed325e332781b9476a66083fb4bdf93747fa
Veyndan Stuart <veyndan@gmail.com>
2023-10-21 22:21:18 +0200

    Don't allow checkers to be moved off the board

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById(`checkers`).childNodes.forEach(checkerElement => {
+Array.from(document.getElementById(`checkers`).children).forEach(checkerElement => {
 	checkerElement.addEventListener(`click`, () => {
 		const dieElement = document.querySelector(`#dice :not(.played)`);
 		dieElement.classList.add(`played`);
@@ -9,6 +9,7 @@ document.getElementById(`checkers`).childNodes.forEach(checkerElement => {
 		const destinationPointCheckerCount = document.querySelectorAll(`use[href="#checker"][data-point="${destinationPointIndex + 1}"]`).length;
 		checkerElement.style.translate = `${(destinationPointIndex < 12 ? -1 : 1) * ((destinationPointIndex % 12 * 40) + (destinationPointIndex % 12 >= 6 ? 50 : 0)) + (destinationPointIndex < 12 ? 490 : 0)}px ${destinationPointIndex < 12 ? (380 - destinationPointCheckerCount * 40) : (destinationPointCheckerCount * 40)}px`;
 		checkerElement.dataset[`point`] = `${destinationPointIndex + 1}`;
+		updateMovabilityOfCheckers(Number(document.querySelector(`#dice :not(.played)`).dataset[`value`]));
 	});
 });
 
@@ -55,6 +56,7 @@ document.getElementById(`roll-dice`).addEventListener(`click`, event => {
 							200,
 						);
 					}
+					updateMovabilityOfCheckers(firstDieValue);
 				}
 			},
 			50,
@@ -63,3 +65,13 @@ document.getElementById(`roll-dice`).addEventListener(`click`, event => {
 
 	repeatedlyRollDice(5);
 });
+
+function updateMovabilityOfCheckers(dieValue) {
+	Array.from(document.getElementById(`checkers`).children).forEach(checkerElement => {
+		const player = Number(checkerElement.dataset[`player`]);
+		const point = Number(checkerElement.dataset[`point`]);
+		const potentialDestinationPoint = point + (player === 1 ? -dieValue : dieValue);
+		const moveable = potentialDestinationPoint >= 1;
+		checkerElement.classList.toggle(`movable`, moveable);
+	});
+}
