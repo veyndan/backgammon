44da17448016ba1d4dccecc1d17daff58248bcb5
Veyndan Stuart <veyndan@gmail.com>
2023-10-21 13:06:42 +0200

    Rename `dieNumber` to `dieValue` to align with associated data attribute

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ document.getElementById(`checkers`).childNodes.forEach(checkerElement => {
 	checkerElement.addEventListener(`click`, () => {
 		const dieElement = document.querySelector(`#dice-holder :first-child`);
 		dieElement.classList.add(`played`);
-		const dieNumber = Number(dieElement.dataset[`value`]);
+		const dieValue = Number(dieElement.dataset[`value`]);
 		const player = Number(checkerElement.dataset[`player`]);
 		const originPointIndex = Number(checkerElement.dataset[`point`]) - 1;
-		const destinationPointIndex = originPointIndex + (player === 1 ? -dieNumber : dieNumber);
+		const destinationPointIndex = originPointIndex + (player === 1 ? -dieValue : dieValue);
 		const destinationPointCheckerCount = document.querySelectorAll(`use[href="#checker"][data-point="${destinationPointIndex + 1}"]`).length;
 		checkerElement.style.translate = `${(destinationPointIndex < 12 ? -1 : 1) * ((destinationPointIndex % 12 * 40) + (destinationPointIndex % 12 >= 6 ? 50 : 0)) + (destinationPointIndex < 12 ? 490 : 0)}px ${destinationPointIndex < 12 ? (380 - destinationPointCheckerCount * 40) : (destinationPointCheckerCount * 40)}px`;
 		checkerElement.dataset[`point`] = `${destinationPointIndex + 1}`;
