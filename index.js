b2d1311b4f121e859fa8189d43e5c40b40419614
veyndan <veyndan@gmail.com>
2024-02-04 17:03:41 +0100

    Specify type of `svgElement`

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ class DieElement {
 	}
 }
 
-const svgElement = document.querySelector(`body > svg`);
+const svgElement = /** @type {SVGSVGElement} */ (document.querySelector(`body > svg`));
 const checkersElement = document.getElementById('checkers');
 const confirmElement = /** @type {HTMLButtonElement} */ (document.getElementById(`confirm`));
 const diceElement = /** @type {SVGSVGElement} */ (document.querySelector(`#dice`));
