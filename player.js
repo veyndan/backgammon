d7e640715f522eea0711c50ae05a28190b1ac302
veyndan <veyndan@gmail.com>
2024-12-21 18:07:04 +0100

    Make player a web component

--- player.js
+++ player.js
@@ -21,3 +21,16 @@ export default class Player {
 		return `Player(${this.value})`;
 	}
 }
+
+customElements.define(
+	"veyndan-player",
+	class extends HTMLElement {
+		constructor() {
+			super();
+			const template = /** @type {HTMLTemplateElement} */ (document.querySelector(`template#player`));
+			this
+				.attachShadow({mode: "open"})
+				.appendChild(template.content.cloneNode(true));
+		}
+	},
+);
